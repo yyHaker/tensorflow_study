358d3a258d70fb53d5dd578972e197710aa0ad93
yyhaker <572176750@qq.com>
2018-04-10 21:21:37 +0800

    learn tensorflow RNN, dropout, BN

--- src/learn/basics/RNN_new.py
+++ src/learn/basics/RNN_new.py
@@ -1,9 +1,16 @@
 # -*- codingL: utf-8 -*-
+"""
+learn to use RNN.
+others:
+tf.reshape()
+TODO: how to realize RNN GRU LSTM and bi-directional  ?
+"""
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
 
+# reproduce
 tf.set_random_seed(1)
 np.random.seed(1)
 
@@ -14,52 +21,58 @@ INPUT_SIZE = 28         # rnn input size / image width
 LR = 0.01               # learning rate
 
 # data
-mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)              # they has been normalized to range (0,1)
+mnist = input_data.read_data_sets(train_dir='./MNIST_data', one_hot=True)    # they has been normalized to range (0,1)
 test_x = mnist.test.images[:2000]
 test_y = mnist.test.labels[:2000]
 
 # plot one example
 print(mnist.train.images.shape)     # (55000, 28 * 28)
 print(mnist.train.labels.shape)   # (55000, 10)
+# show an image
 plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
 plt.title('%i' % np.argmax(mnist.train.labels[0]))
 plt.show()
 
 # tensorflow placeholders
-tf_x = tf.placeholder(tf.float32, [None, TIME_STEP * INPUT_SIZE])       # shape(batch, 784)
-image = tf.reshape(tf_x, [-1, TIME_STEP, INPUT_SIZE])                   # (batch, height, width, channel)
-tf_y = tf.placeholder(tf.int32, [None, 10])                             # input y
+tf_x = tf.placeholder(dtype=tf.float32, shape=[None, TIME_STEP * INPUT_SIZE])       # shape(batch, 784)
+image = tf.reshape(tf_x, [-1, TIME_STEP, INPUT_SIZE])          # shape(batch, height, width)
+tf_y = tf.placeholder(dtype=tf.int32, shape=[None, 10])          # input y
 
-# RNN
+# build the network: a RNN layer + linear layer
 rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_units=64)
 outputs, (h_c, h_n) = tf.nn.dynamic_rnn(
-    rnn_cell,                   # cell you have chosen
-    image,                      # input
-    initial_state=None,         # the initial hidden state
+    cell=rnn_cell,                   # cell you have chosen
+    inputs=image,                      # input
+    initial_state=None,         # the initial hidden state  (how to initialize?)
     dtype=tf.float32,           # must given if set initial_state = None
     time_major=False,           # False: (batch, time step, input); True: (time step, batch, input)
 )
-output = tf.layers.dense(outputs[:, -1, :], 10)              # output based on the last output step
+# add a neural layer
+output = tf.layers.dense(inputs=outputs[:, -1, :], units=10)              # output based on the last output step
 
-loss = tf.losses.softmax_cross_entropy(onehot_labels=tf_y, logits=output)           # compute cost
+# compute cost
+loss = tf.losses.softmax_cross_entropy(onehot_labels=tf_y, logits=output)
 train_op = tf.train.AdamOptimizer(LR).minimize(loss)
 
-accuracy = tf.metrics.accuracy(          # return (acc, update_op), and create 2 local variables
-    labels=tf.argmax(tf_y, axis=1), predictions=tf.argmax(output, axis=1),)[1]
+# return (acc, update_op), and create 2 local variables
+accuracy = tf.metrics.accuracy(
+    labels=tf.argmax(tf_y, axis=1), predictions=tf.argmax(output, axis=1))[1]
 
+# create session
 sess = tf.Session()
 init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()) # the local var is for accuracy_op
 sess.run(init_op)     # initialize var in graph
 
-for step in range(1200):    # training
+# begin training
+for step in range(1200):
     b_x, b_y = mnist.train.next_batch(BATCH_SIZE)
-    _, loss_ = sess.run([train_op, loss], {tf_x: b_x, tf_y: b_y})
+    _, loss_ = sess.run([train_op, loss], feed_dict={tf_x: b_x, tf_y: b_y})
     if step % 50 == 0:      # testing
-        accuracy_ = sess.run(accuracy, {tf_x: test_x, tf_y: test_y})
+        accuracy_ = sess.run(accuracy, feed_dict={tf_x: test_x, tf_y: test_y})
         print('train loss: %.4f' % loss_, '| test accuracy: %.2f' % accuracy_)
 
 # print 10 predictions from test data
-test_output = sess.run(output, {tf_x: test_x[:10]})
-pred_y = np.argmax(test_output, 1)
+test_output = sess.run(output, feed_dict={tf_x: test_x[:10]})
+pred_y = np.argmax(test_output, axis=1)    # use the max value to predict
 print(pred_y, 'prediction number')
-print(np.argmax(test_y[:10], 1), 'real number')
+print(np.argmax(test_y[:10], axis=1), 'real number')
--- src/learn/basics/dropout.py
+++ src/learn/basics/dropout.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+use dropout layer:
+tf.layers.dropout()
+"""
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# reproduce
 tf.set_random_seed(1)
 np.random.seed(1)
 
@@ -12,59 +17,72 @@ N_HIDDEN = 300
 LR = 0.01
 
 # training data
-x = np.linspace(-1, 1, N_SAMPLES)[:, np.newaxis]  # (20, 1)
-y = x + 0.3*np.random.randn(N_SAMPLES)[:, np.newaxis]
+x = np.linspace(-1, 1, N_SAMPLES)[:, np.newaxis]  # shape(20, 1)
+y = x + 0.3*np.random.randn(N_SAMPLES)[:, np.newaxis]    # add some noise data
 
 # test data
-test_x = x.copy()
+test_x = x.copy()       # shape (20, 1)
 test_y = test_x + 0.3*np.random.randn(N_SAMPLES)[:, np.newaxis]
 
 # show data
-plt.scatter(x, y, c='magenta', s=50, alpha=0.5, label='train')
-plt.scatter(test_x, test_y, c='cyan', s=50, alpha=0.5, label='test')
+plt.scatter(x=x, y=y, c='magenta', s=50, alpha=0.5, label='train')
+plt.scatter(x=test_x, y=test_y, c='cyan', s=50, alpha=0.5, label='test')
 plt.legend(loc='upper left')
 plt.ylim((-2.5, 2.5))
 plt.show()
 
 # tf placeholders
-tf_x = tf.placeholder(tf.float32, [None, 1])
-tf_y = tf.placeholder(tf.float32, [None, 1])
-tf_is_training = tf.placeholder(tf.bool, None)  # to control dropout when training and testing
+tf_x = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+tf_y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+# to control dropout when training and testing
+tf_is_training = tf.placeholder(dtype=tf.bool, shape=None)
 
 # overfitting net
-o1 = tf.layers.dense(tf_x, N_HIDDEN, tf.nn.relu)
-o2 = tf.layers.dense(o1, N_HIDDEN, tf.nn.relu)
-o_out = tf.layers.dense(o2, 1)
-o_loss = tf.losses.mean_squared_error(tf_y, o_out)
+o1 = tf.layers.dense(inputs=tf_x, units=N_HIDDEN, activation=tf.nn.relu)
+o2 = tf.layers.dense(inputs=o1, units=N_HIDDEN, activation=tf.nn.relu)
+o_out = tf.layers.dense(inputs=o2, units=1)
+# compute loss
+o_loss = tf.losses.mean_squared_error(labels=tf_y, predictions=o_out)
 o_train = tf.train.AdamOptimizer(LR).minimize(o_loss)
 
 # dropout net
-d1 = tf.layers.dense(tf_x, N_HIDDEN, tf.nn.relu)
-d1 = tf.layers.dropout(d1, rate=0.5, training=tf_is_training)   # drop out 50% of inputs
-d2 = tf.layers.dense(d1, N_HIDDEN, tf.nn.relu)
-d2 = tf.layers.dropout(d2, rate=0.5, training=tf_is_training)   # drop out 50% of inputs
-d_out = tf.layers.dense(d2, 1)
-d_loss = tf.losses.mean_squared_error(tf_y, d_out)
+d1 = tf.layers.dense(inputs=tf_x, units=N_HIDDEN, activation=tf.nn.relu)
+d1 = tf.layers.dropout(inputs=d1, rate=0.5, training=tf_is_training)   # drop out 50% of inputs
+d2 = tf.layers.dense(inputs=d1, units=N_HIDDEN, activation=tf.nn.relu)
+d2 = tf.layers.dropout(inputs=d2, rate=0.5, training=tf_is_training)   # drop out 50% of inputs
+d_out = tf.layers.dense(inputs=d2, units=1)
+# compute loss
+d_loss = tf.losses.mean_squared_error(labels=tf_y, predictions=d_out)
 d_train = tf.train.AdamOptimizer(LR).minimize(d_loss)
 
+# create session
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 plt.ion()   # something about plotting
 
 for t in range(500):
-    sess.run([o_train, d_train], {tf_x: x, tf_y: y, tf_is_training: True})  # train, set is_training=True
+    # train
+    sess.run([o_train, d_train], feed_dict={tf_x: x, tf_y: y, tf_is_training: True})  # train, set is_training=True
 
     if t % 10 == 0:
         # plotting
-        plt.cla()
+        plt.cla()  # clear the axis
+        # test, set is_training=False
         o_loss_, d_loss_, o_out_, d_out_ = sess.run(
-            [o_loss, d_loss, o_out, d_out], {tf_x: test_x, tf_y: test_y, tf_is_training: False} # test, set is_training=False
+            [o_loss, d_loss, o_out, d_out], feed_dict={tf_x: test_x, tf_y: test_y, tf_is_training: False}
         )
-        plt.scatter(x, y, c='magenta', s=50, alpha=0.3, label='train'); plt.scatter(test_x, test_y, c='cyan', s=50, alpha=0.3, label='test')
-        plt.plot(test_x, o_out_, 'r-', lw=3, label='overfitting'); plt.plot(test_x, d_out_, 'b--', lw=3, label='dropout(50%)')
-        plt.text(0, -1.2, 'overfitting loss=%.4f' % o_loss_, fontdict={'size': 20, 'color':  'red'}); plt.text(0, -1.5, 'dropout loss=%.4f' % d_loss_, fontdict={'size': 20, 'color': 'blue'})
-        plt.legend(loc='upper left'); plt.ylim((-2.5, 2.5)); plt.pause(0.1)
+        # plot the data
+        plt.scatter(x=x, y=y, c='magenta', s=50, alpha=0.3, label='train')
+        plt.scatter(x=test_x, y=test_y, c='cyan', s=50, alpha=0.3, label='test')
+
+        plt.plot(test_x, o_out_, 'r-', lw=3, label='overfitting')
+        plt.plot(test_x, d_out_, 'b--', lw=3, label='dropout(50%)')
+        plt.text(0, -1.2, 'overfitting loss=%.4f' % o_loss_, fontdict={'size': 20, 'color':  'red'})
+        plt.text(0, -1.5, 'dropout loss=%.4f' % d_loss_, fontdict={'size': 20, 'color': 'blue'})
+        plt.legend(loc='upper left')
+        plt.ylim((-2.5, 2.5))
+        plt.pause(0.1)
 
 plt.ioff()
 plt.show()
